7116f146989d237d34dee0b20e20324af8684405
phong.vh <phong.vh@kiotviet.com>
2023-07-18 11:55:13 +0700

    #feature: static-image

--- sample-multi-widget-test-react-typescript-vite-react-query/src/pages/page1/page-container.tsx
+++ sample-multi-widget-test-react-typescript-vite-react-query/src/pages/page1/page-container.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+const viteIcon = new URL('/public/vite.svg', import.meta.url).href
 
 function PageContainer() {
-  return <h1 data-testid="page-container">Page 1</h1>
+  return (
+    <div>
+      <img src={viteIcon} alt="vite-icon" />
+      <h1 data-testid="page-container">Page 1</h1>
+    </div>
+  )
 }
 
 export default PageContainer
