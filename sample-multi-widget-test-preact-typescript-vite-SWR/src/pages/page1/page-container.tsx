74f6954e9870a7104f3872a1ac6d7f7858bdb4d4
phongvh <phongvh1405@gmail.com>
2024-10-13 12:50:52 +0700

    feat: remove preact ver

--- sample-multi-widget-test-preact-typescript-vite-SWR/src/pages/page1/page-container.tsx
+++
@@ -1,5 +0,0 @@
-function PageContainer(props: any) {
-  return <h1 {...props}>Page 1</h1>;
-}
-
-export default PageContainer;
--- sample-multi-widget-test-preact-typescript-vite-SWR/src/pages/page2/page-container.tsx
+++
@@ -1,5 +0,0 @@
-function PageContainer(props: any) {
-  return <h1 {...props}>Page2</h1>;
-}
-
-export default PageContainer;
